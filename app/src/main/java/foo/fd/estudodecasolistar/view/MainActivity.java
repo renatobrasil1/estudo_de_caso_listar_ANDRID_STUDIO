8dbc17bf4d493deace160a2acc2f145394394001
renatobrasil1 <nato-brasil@hotmail.com>
2023-11-16 16:49:04 -0300

    concluida Aula 34 da sessão 3:2 implementado AsynTask

--- app/src/main/java/foo/fd/estudodecasolistar/view/MainActivity.java
+++ app/src/main/java/foo/fd/estudodecasolistar/view/MainActivity.java
@@ -12,8 +12,13 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
 
 import foo.fd.estudodecasolistar.R;
 
@@ -27,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         ListarEstadosAsyncTask task  = new ListarEstadosAsyncTask("fabricadedesenvolvedor");
-
         task.execute();
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -62,51 +66,119 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-         // criando uma classe assicrona
+    // criando uma classe assicrona
 
     public class ListarEstadosAsyncTask
             extends
             AsyncTask<String, String, String> {
 
-        String api_token;
+        String api_token, query;
 
         HttpURLConnection conn;
         URL url = null;
         Uri.Builder builder;
-
-        final String URL_WEB_SERVICE = "http://192.168.1.7/Documents/cursoudemyweb/APIListarEstados.php";
+        final String URL_WEB_SERVICES = "http://192.168.0.107/Documents/cursoudemyweb/APIListarEstados.php";
         final int READ_TIMEOUT = 10000; // MILISSEGUNDOS
         final int CONNECTION_TIMEOUT = 30000;
+
+        int response_code;
         public ListarEstadosAsyncTask(String token){
             this.api_token = token;
             this. builder = new Uri.Builder();
             builder.appendQueryParameter("api_token",api_token);
 
-
-
         }
 
         @Override
-        protected void onPreExecute(){
-
-            Log.i("APIListar","onPreExecute()");
-
+        protected void onPreExecute(){Log.i("APIListar","onPreExecute()");
         }
 
         @Override
         protected String doInBackground(String... strings) {
 
             Log.i("APIListar","doInBackground()");
-            return "Processado com sucesso";
+
+            //Gerar o conteúdo para a URL
+
+            try{
+
+                url = new URL(URL_WEB_SERVICES);
+
+            }catch (MalformedURLException e){
+                Log.i("APIListar","doInBackground()-->"+e.getMessage());
+
+            }catch(Exception e){
+
+                Log.i("APIListar","doInBackground()-->"+e.getMessage());
+            }
+
+            // Gerar uma requisição HTTP - POST - result será um ArryaJson
+            // conn
+
+            try {
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(READ_TIMEOUT);
+                conn.setConnectTimeout(CONNECTION_TIMEOUT);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("charset","utf_8");
+
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                conn.connect();
+
+            }catch(Exception e){
+                Log.i("APIListar","doInBackground() --> "+e.getMessage());
+
+            }
+
+            // Adicionar o TOKEN e/ou outros parametros como por exemplo
+            // um objeto a ser incluido, deletado ou alterado.
+            // CRUD comleto
+
+            try {
+
+                query = builder.build().getEncodedQuery();
+                OutputStream stream = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(stream,"utf-8"));
+
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                stream.close();
+
+                conn.connect();
+
+            }catch(Exception e){
+
+                Log.i("APIListar","doInBackground()-->"+e.getMessage());
+
+            }
+
+            // Receber o response - arryJson
+            // http - código do response  | 200 | 404 | 503
+
+
+            try{
+
+                response_code = conn.getResponseCode();
+
+            }catch(Exception e){
+                Log.i("APIListar","doInBackground()-->"+e.getMessage());
+
+            }
+
+            return "Processameto com sucesso";
+
 
         }
 
         @Override
         protected void onPostExecute(String result){
 
-            Log.i("APIListar","onPostExecute()-----> Result "+ result );
+            Log.i("APIListar","onPostExecute()-----> Result "+result );
 
         }
     }
-
 }
